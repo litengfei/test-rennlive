97a7fad1ad8f7f07c0ba70aad465f8c2fd04770a
李腾飞 <tengfei.li@renren-inc.com>
2015-12-18 10:09:48 +0800

    add call ffmpeg lib test code

--- recorder/app/src/main/cpp/ijkplayer-k0.4.4_ffmpeg-n2.8.3/jni/rennlive/rennlivejni.c
+++ recorder/app/src/main/cpp/ijkplayer-k0.4.4_ffmpeg-n2.8.3/jni/rennlive/rennlivejni.c
@@ -5,11 +5,135 @@
 #include <assert.h>
 #include <jni.h>
 #include <pthread.h>
+
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+
 #include "rennlivejni.h"
 #include "renn_common_macros.h"
 #include "renn_log.h"
 
 static JavaVM *g_jvm;
+
+
+
+
+//FIX
+struct URLProtocol;
+/**
+ * Protocol Support Information
+ */
+char * urlprotocolinfo(){
+
+    char info[40000]={0};
+    av_register_all();
+
+    struct URLProtocol *pup = NULL;
+    //Input
+    struct URLProtocol **p_temp = &pup;
+    avio_enum_protocols((void **)p_temp, 0);
+    while ((*p_temp) != NULL){
+        sprintf(info, "%s[In ][%10s]\n", info, avio_enum_protocols((void **)p_temp, 0));
+    }
+    pup = NULL;
+    //Output
+    avio_enum_protocols((void **)p_temp, 1);
+    while ((*p_temp) != NULL){
+        sprintf(info, "%s[Out][%10s]\n", info, avio_enum_protocols((void **)p_temp, 1));
+    }
+
+    return info;
+}
+
+/**
+ * AVFormat Support Information
+ */
+char * avformatinfo(){
+
+    char info[40000] = { 0 };
+
+    av_register_all();
+
+    AVInputFormat *if_temp = av_iformat_next(NULL);
+    AVOutputFormat *of_temp = av_oformat_next(NULL);
+    //Input
+    while(if_temp!=NULL){
+        sprintf(info, "%s[In ] %10s\n", info, if_temp->name);
+        if_temp=if_temp->next;
+    }
+    //Output
+    while (of_temp != NULL){
+        sprintf(info, "%s[Out] %10s\n", info, of_temp->name);
+        of_temp = of_temp->next;
+    }
+    return info;
+}
+
+/**
+ * AVCodec Support Information
+ */
+char * avcodecinfo()
+{
+    char info[40000] = { 0 };
+
+    av_register_all();
+
+    AVCodec *c_temp = av_codec_next(NULL);
+
+    while(c_temp!=NULL){
+        if (c_temp->decode!=NULL){
+            sprintf(info, "%s[Dec]", info);
+        }
+        else{
+            sprintf(info, "%s[Enc]", info);
+        }
+        switch (c_temp->type){
+            case AVMEDIA_TYPE_VIDEO:
+                sprintf(info, "%s[Video]", info);
+                break;
+            case AVMEDIA_TYPE_AUDIO:
+                sprintf(info, "%s[Audio]", info);
+                break;
+            default:
+                sprintf(info, "%s[Other]", info);
+                break;
+        }
+
+        sprintf(info, "%s %10s\n", info, c_temp->name);
+
+        c_temp=c_temp->next;
+    }
+    return info;
+}
+
+/**
+ * AVFilter Support Information
+ */
+char * avfilterinfo()
+{
+    char info[40000] = { 0 };
+    av_register_all();
+    AVFilter *f_temp = (AVFilter *)avfilter_next(NULL);
+    while (f_temp != NULL){
+        sprintf(info, "%s[%10s]\n", info, f_temp->name);
+    }
+    return info;
+}
+
+/**
+ * Configuration Information
+ */
+char * configurationinfo()
+{
+    char info[10000] = { 0 };
+    av_register_all();
+
+    sprintf(info, "%s\n", avcodec_configuration());
+
+    return info;
+}
+
 
 
 
@@ -38,8 +162,15 @@ static JNINativeMethod g_methods[] = {
 JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
     ALOGE("JNI_OnLoad() called");
+
+
+ALOGE("%s",configurationinfo());
+ALOGE("%s",urlprotocolinfo());
+ALOGE("%s",avformatinfo());
+ALOGE("%s",avcodecinfo());
+ALOGE("%s",avfilterinfo());
+
     JNIEnv* env = NULL;
-__android_log_print(ANDROID_LOG_ERROR, RENN_LOG_TAG, "JNI_OnLoad() called");
     g_jvm = vm;
     if ((*vm)->GetEnv(vm, (void**) &env, JNI_VERSION_1_4) != JNI_OK) {
         return -1;
